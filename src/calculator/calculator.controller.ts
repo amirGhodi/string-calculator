384f71f12568937677eb85b480eb7e6a6e71a0b3
amirGhodi <amirgodi786@gmail.com>
2024-07-08 16:56:53 +0530

    added api support

--- src/calculator/calculator.controller.ts
+++ src/calculator/calculator.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { CalculatorService } from './calculator.service';
 
 @Controller('calculator')
-export class CalculatorController {}
+export class CalculatorController {
+  constructor(private readonly calculatorService: CalculatorService) {}
+
+  @Post('add')
+  add(@Body('numbers') numbers: string): number {
+    try {
+      return this.calculatorService.add(numbers);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
